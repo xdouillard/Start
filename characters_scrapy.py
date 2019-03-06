dc6aef1a584048b2f975e66b143bd8002ddd55bd
Xavier Douillard <xavier.douillard@gmail.com>
2019-03-06 08:08:05 +0100

    Configuration de characters_scrpy.json

--- characters_scrapy.py
+++ characters_scrapy.py
@@ -1,12 +1,7 @@
-import scrapy
-
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
+    start_urls = ['https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Personnage_d%27animationm']
 
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
-
-        for next_page in response.css('div.prev-post > a'):
-            yield response.follow(next_page, self.parse)
+        for title in response.css('div#mw-pages li'):
+            yield {'character': title.css('a ::text').extract_first()}
